d45a32ab44e4777f6fb8683667c8fb52a71c3ffe
spriteray <spriteray@gmail.com>
2021-04-12 14:15:25 +0800

    dircolors
    Signed-off-by: spriteray <spriteray@gmail.com>

--- install.sh
+++ install.sh
@@ -14,3 +14,11 @@ cp -a _gitconfig ~/.gitconfig
 
 cp xssh ~/bin/
 cp _xsshrc ~/.xsshrc
+
+#if [ -x /usr/bin/dircolors ]; then
+#	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+#	alias ls='ls --color=auto'
+#	alias grep='grep --color=auto'
+#	alias fgrep='fgrep --color=auto'
+#	alias egrep='egrep --color=auto'
+#fi
