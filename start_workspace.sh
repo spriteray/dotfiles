f03bc962f32930254764ea8f97dbea7869777379
lei.zhang <lei.zhang@silverkeyz.com>
2022-12-16 19:13:21 +0800

    update
    Signed-off-by: lei.zhang <lei.zhang@silverkeyz.com>

--- start_workspace.sh
+++ start_workspace.sh
@@ -7,6 +7,7 @@ window_list="console,bin protocol,protocol fight,scripts/fight simulate,tools/si
 # 打开会话
 tmux new-session -s $session_name -n editor -d
 tmux send-keys -t $session_name 'cd ~/magicserver' C-m
+tmux send-keys -t $session_name 'vim' C-m
 
 window_id=1
 for w in $window_list; do
@@ -19,8 +20,9 @@ for w in $window_list; do
 	if [ "$name" == "console" ]; then
 		# 分割
 		tmux split-window -h -p 10 -t $session_name:$window_id
-		tmux split-window -v -t $session_name:$window_id.2
+		tmux split-window -v -p 10 -t $session_name:$window_id.2
 		tmux send-keys -t $session_name:$window_id.2 "$cmd" C-m
+		tmux send-keys -t $session_name:$window_id.2 "./top.sh" C-m
 		tmux send-keys -t $session_name:$window_id.3 "$cmd" C-m
 	fi
 done
