112196152b1162d7c229dbf87b8f4e8c0ce2feed
lei.zhang <lei.zhang@silverkeyz.com>
2023-05-01 01:13:02 +0800

    移除nvim配色模块，直接放入init
    Signed-off-by: lei.zhang <lei.zhang@silverkeyz.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,20 @@
 
+local colorscheme = 'monokai_pro'
 -- cpp文件类型列表
-cppfilelist = { 'cpp', 'c', 'h', 'cc', 'hpp' }
+local cppfilelist = { 'cpp', 'c', 'h', 'cc', 'hpp' }
 -- 插件根目录
-pluginpath = vim.fn.stdpath( 'data' ) .. '/plugin'
+local pluginpath = vim.fn.stdpath( 'data' ) .. '/plugin'
 
--- 加载配置
+-- 加载基础配置
+require('basic')
+
+-- 加载插件
 local plugins = require('plugins')
 plugins.init( pluginpath )
 plugins.load( pluginpath, cppfilelist )
 
--- 加载其他选项
-require('basic')
-require('colorscheme')
+-- 应用配色
+local is_ok,_ = pcall( vim.cmd, "colorscheme "..colorscheme )
+if not is_ok then
+    vim.notify( 'colorscheme ' .. colorscheme .. ' not found !' )
+end
